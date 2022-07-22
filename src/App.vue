a5b38c71a419be4b129f62fcb3400460149411dc
Behzod Asliddinov <behzodme@gmail.com>
2022-07-22 23:15:15 +0500

    'behzod'

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
+  
+ <Loading v-if="loading"></Loading>
+  <div v-if="!loading">
   <Navbar />
+   
   <div class="section">
   <router-view :key="$route.fullPath" />
   </div>
@@ -12,14 +15,30 @@
 <style lang="scss">
 
 @import '../node_modules/bulma';
+
 </style>
 <script>
 import Footer from './components/Footer.vue'
 import Navbar from './components/Navbar.vue'
+import Loading from '@/components/Loading.vue'
+
 export default {
   name:'App',
   components:{
-    Navbar,Footer
+    Navbar,
+    Footer,
+    Loading,
+    Loading
+},
+data(){
+  return {
+    loading:true
+  }
+},
+mounted() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 1500);
   }
 }
 </script>
---
+++ src/components/Loading.vue
@@ -0,0 +1,35 @@
+<template>
+<div id="loader">
+    <img
+      class="img"
+      src="../assets/loading.gif"
+      alt="loading..."
+      width="200"
+    />
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Loading'
+}
+</script>
+
+<style scoped>
+#loading {
+  position: relative;
+  z-index: 1000;
+  background-color: white;
+  height: 100vh;
+  width: 100vw;
+  text-align: center;
+  line-height: 100vh;
+}
+.img {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+
+}
+</style>
